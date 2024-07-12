2902b402d4db8e507404f6276c0c843c3a19c01b
Anikcse18 <istiakjavedanik@gmail.com>
2024-07-12 17:55:59 +0600

    added featured pets section

---
+++ test.js
@@ -0,0 +1,45 @@
+<!-- features Pet -->
+<section>
+    <!-- features text -->
+    <div class="">
+        <h1 class="text-3xl fredoka font-semibold text-center">Featured Pets </h1>
+        <p class="text-center text-sm mt-2 text-secondary"> Lorem ipsum dolor sit amet, est vide voluptaria ex,
+            nec in hinc
+            solum
+            sat. <br>
+            Neceessitatibus sonet
+            soluta,
+            vim eu esse accusamus.</p>
+
+    </div>
+    <!-- Features Picture -->
+    <div class="flex flex-col lg:flex-row ">
+
+        <div class="card text-primary-content w-96">
+            <div class="card-body relative">
+                <img src="./img/product_bg_1.png" alt="" srcset="">
+                <img src="./img/product_1.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
+            </div>
+        </div>
+        <div class="card text-primary-content w-96">
+            <div class="card-body relative">
+                <img src="./img/product_bg_2.png" alt="" srcset="">
+                <img src="./img/product_2.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
+            </div>
+        </div>
+        <div class="card text-primary-content w-96">
+            <div class="card-body relative">
+                <img src="./img/product_bg_3.png" alt="" srcset="">
+                <img src="./img/product_3.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
+            </div>
+        </div>
+        <div class="card text-primary-content w-96">
+            <div class="card-body relative">
+                <img src="./img/product_bg_4.png" alt="" srcset="">
+                <img src="./img/product_4.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
+            </div>
+        </div>
+
+
+    </div>
+</section>
