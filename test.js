fe5beaa00d4bf033294fde9f18b446bcc475534b
Anikcse18 <istiakjavedanik@gmail.com>
2024-07-13 17:45:27 +0600

    added feature services

--- test.js
+++ test.js
@@ -1,45 +1,31 @@
-<!-- features Pet -->
-<section>
-    <!-- features text -->
-    <div class="">
-        <h1 class="text-3xl fredoka font-semibold text-center">Featured Pets </h1>
-        <p class="text-center text-sm mt-2 text-secondary"> Lorem ipsum dolor sit amet, est vide voluptaria ex,
-            nec in hinc
-            solum
-            sat. <br>
-            Neceessitatibus sonet
-            soluta,
-            vim eu esse accusamus.</p>
-
-    </div>
-    <!-- Features Picture -->
-    <div class="flex flex-col lg:flex-row ">
-
-        <div class="card text-primary-content w-96">
-            <div class="card-body relative">
-                <img src="./img/product_bg_1.png" alt="" srcset="">
-                <img src="./img/product_1.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
-            </div>
-        </div>
-        <div class="card text-primary-content w-96">
-            <div class="card-body relative">
-                <img src="./img/product_bg_2.png" alt="" srcset="">
-                <img src="./img/product_2.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
-            </div>
-        </div>
-        <div class="card text-primary-content w-96">
-            <div class="card-body relative">
-                <img src="./img/product_bg_3.png" alt="" srcset="">
-                <img src="./img/product_3.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
-            </div>
-        </div>
-        <div class="card text-primary-content w-96">
-            <div class="card-body relative">
-                <img src="./img/product_bg_4.png" alt="" srcset="">
-                <img src="./img/product_4.png" alt="" srcset="" class="absolute inset-0 m-auto w-1/2">
-            </div>
-        </div>
+<div class="flex-1">
+                    <div class=" h-full w-full relative">
+                        <div class=" bg-[#F9A6AC] lg:w-[458px] w-[300px] h-[280px] lg:h-[350px] rounded-lg relative">
+                            <img src="./img/Pattern_right.png" alt="" srcset=""
+                                class="hidden lg:block absolute -right-10 w-10">
+                            <img src="./img/Left_pattern.png" alt="" srcset=""
+                                class="hidden lg:block absolute bottom-0 -left-10 w-10">
+                            <!-- Cart -->
+                            <div
+                                class="w-28 lg:w-32 h-28 lg:h-32 rounded-lg fredoka text-center text-white bg-[#7EC1F7] absolute left-42 lg:-left-20  -top-14">
+                                <div
+                                    class="text-center flex flex-col justify-center items-center mt-3 gap-y-3 relative">
+                                    <h1 class="text-2xl font-medium inline-block">50 <span
+                                            class="text-sm -ml-1">+</span>
+                                    </h1>
+                                    <h1 class="font-medium">Service</h1>
+                                    <p class="font-medium text-xs">Aviable</p>
+                                    <div class="absolute -right-16 -top-16">
+                                        <img src="./img/Highlight Effect Line 1.png" alt="" srcset="">
+                                    </div>
+                                </div>
 
 
-    </div>
-</section>
+                            </div>
+                            <img src="./img/female_with_cat.png" alt="" srcset=""
+                                class="lg:w-[361px] w-[250px] lg:h-[487px] h-[380px] absolute bottom-0 left-[8%] lg:left-[10%]">
+                        </div>
+
+                    </div>
+
+                </div>
